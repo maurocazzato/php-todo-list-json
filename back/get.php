fe316af9a621813888d1a2cbe4984a0f1e664fad
Maurocazzato <maurizzio.125@gmail.com>
2024-01-29 00:01:16 +0100

    esercizio(non pienamente funzionante)

---
+++ back/get.php
@@ -0,0 +1,24 @@
+<?php
+
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Headers: X-Requested-With");
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+
+    
+    $posts = [
+        [
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, eget aliquam nis',
+            'date' => '01/01/2020',
+        ],
+        [
+            'text' => 'Hello World',
+            'date' => '02/01/2020',
+        ],
+        [
+            'text' => 'Ciao Mondo',
+            'date' => '03/01/2020',
+        ]
+    ];
+
+    $jsonPosts = json_encode($posts);
+    echo $jsonPosts;
